8ca5626c1f769d272bf9dae09e54e963ae5c8e35
Rozmarynowska Monika <rm44478>
2020-01-19 19:22:41 +0100

    Add creation of file name

--- lab13.c
+++ lab13.c
@@ -10,6 +10,51 @@
 #include <unistd.h> //getopt()
 #include <time.h> //ctime()
 
+#include <stdio.h>
+
+struct Log
+{
+	struct tm date;
+	char argument[10];
+};
+
+//nie sprawdzam, czy nie przekroczylam tablicy (czy nie pisze po pamieci)!!
+void IntToStr(int n, char * result)
+{
+	int temporaryIterator = 0;
+	int resultIterator = 0;
+	char temp[10];
+	
+	if (n < 0)
+	{
+		result[resultIterator++] = '-';
+		n = -n;
+	}
+	 if (n >= 0 && n <= 9)
+	{
+		result[resultIterator++] = '0';
+		result[resultIterator++] = n % 10 + 48;
+		result[resultIterator] = '\0';
+		return;
+	}
+	do
+	{
+		temp[temporaryIterator] = n % 10 + 48;
+		temporaryIterator++;
+		
+		n = n - n % 10;
+		n = n / 10;
+	} while (n);
+	while(temporaryIterator>=0)
+	{
+		
+		result[resultIterator] = temp[temporaryIterator-1];
+		resultIterator++;
+		temporaryIterator--;
+	}
+	result[resultIterator] = '\0';
+}
+
 int main(int argc, char *argv[])
 {
 	int opt = getopt(argc, argv, ":w:r");
@@ -34,7 +79,70 @@ int main(int argc, char *argv[])
 	}
 	if (opt == 'w')
 	{
-		write(1, optarg, strlen(optarg));
+		struct Log log;
+		struct tm* date;
+		//write(1, optarg, strlen(optarg));
+		//przygotowanie argumentu
+		strncpy(log.argument, optarg, 9);
+		log.argument[9] = '\0';
+		//write(1, log.argument, strlen(log.argument));
+
+		//pobranie aktualnego czasu
+		time_t rawTime;
+		time(&rawTime);
+		date = localtime(&rawTime);
+		log.date = *date;
+		/*printf("Current local time and date: %s", asctime(&(log.date)));*/
+		//printf("%d", date->tm_year+1900);
+
+		//tworzenie napisu z nazwna szukanego pliku
+		char* fileName = malloc(sizeof(char) * 21);
+		char* buffer = malloc(sizeof(char) * 10);
+		char* dot = malloc(sizeof(char) * 2);
+		char* extension = malloc(sizeof(char) * 4);
+
+		dot[0] = '.';
+		dot[1] = '\0';
+
+		extension[0] = 'l';
+		extension[1] = 'o';
+		extension[2] = 'g';
+		extension[3] = '\0';
+
+		//rok
+		IntToStr(date->tm_year+1900, buffer);
+		strcat(fileName, buffer);
+		strcat(fileName, dot);
+
+		//miesiac
+		IntToStr(date->tm_mon + 1, buffer);
+		strcat(fileName, buffer);
+		strcat(fileName, dot);
+
+		//dzien
+		IntToStr(date->tm_mday, buffer);
+		strcat(fileName, buffer);
+		strcat(fileName, dot);
+
+		//godzina
+		IntToStr(date->tm_hour, buffer);
+		strcat(fileName, buffer);
+		strcat(fileName, dot);
+
+		//minuty
+		IntToStr(date->tm_min, buffer);
+		strcat(fileName, buffer);
+		strcat(fileName, dot);
+
+		//rozszerzenie
+		strcat(fileName, extension);
+		//printf("%s", fileName);
+
+
+
+		free(fileName);
+		free(buffer);
+		free(dot);
 	}
 	if (opt == 'r')
 	{
